6d9d71d9403ec75e3aa6a9635c10d063b6d622ea
Burhan Mullamitha <burhanmm1998@gmail.com>
2020-03-24 04:26:58 +0000

    Initialized Linked list implementation class and bind it to RMI

--- VehicleMgmtSystem/src/Server.java
+++ VehicleMgmtSystem/src/Server.java
@@ -1,14 +1,24 @@
+import java.rmi.Naming;
 import java.rmi.registry.LocateRegistry;
 
 public class Server {
 
+	public static LinkedListImplementation linkedlistImpl;
+	
 	public static void main(String args[]) {
 		try {
 			LocateRegistry.createRegistry(1099);
 			System.out.println("RMI REGISTRY CREATED");
 
 		} catch (Exception e) {
-
+			
+		}
+		try {
+			linkedlistImpl = new LinkedListImplementation();
+			Naming.rebind("list", linkedlistImpl);
+		}
+		catch(Exception e) {
+			System.out.println(e);
 		}
 	}
 
